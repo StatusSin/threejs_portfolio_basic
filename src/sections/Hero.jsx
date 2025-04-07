ce35b883478d4a544bcf459cbe4b638e8a48abf7
Aryaman Ramchandran <aryamanramchandran@gmail.com>
2025-04-06 22:47:07 -0500

    Hero Section Setup
    Setup the hero section with name text on screen

---
+++ src/sections/Hero.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Hero = () => {
+    return (
+        <section className = "min-h-screen w-full flex flex-col relative">
+            <div className = "w-full mx-auto flex flex-col lg:mt-36 mt-20 c-space gap-3">
+                <p className = "lg:text-3xl text-2xl font-medium text-white text-center font">Hello, I am Aryaman<span className = "waving-hand">👋</span></p>
+            </div>
+        </section>
+    )
+}
+export default Hero
